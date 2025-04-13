a233d1e974c4aa64142b26d68216143a4743916b
IniCang <iehua.surya@gmail.com>
2025-04-14 00:02:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+function changeMessage() {
+    const message = document.getElementById('message');
+    message.textContent = "You just made my day 😄";
+  }
+  
